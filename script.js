6ff95e46fdb982f69e840f51e20b570f24972a4c
acrubaugh11 <abcrubaugh@uncg.edu>
2025-10-06 23:16:39 -0400

    fixed api handling

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 'use strict';
 //  https://api.github.com/users/USERNAME/repos
-
-
-
 async function getRepos() {
     try {
         let res = await fetch("https://api.github.com/users/acrubaugh11/repos");
@@ -11,11 +8,19 @@ async function getRepos() {
         }
         res = await res.json();
         console.log("Repos resolved: My Repo Data:", res)
+        let galleryItem = document.getElementsByClassName('repo');
+        let name = res[0].name;
+        let nameNode = document.createTextNode(name);
+        galleryItem[0].appendChild(nameNode);
+
+
+
     }
     catch (err) {
         console.log("Error:", err);
 
     }
 }
+
 
 getRepos();
